7432b7c3a64d264abb5f82d160ac00169af8b2fc
CHIKI <108464117+oCHIKIo@users.noreply.github.com>
2024-12-20 21:59:55 +0100

    Update ft_lstlast_bonus.c

--- not_your_libft/ft_lstlast_bonus.c
+++ not_your_libft/ft_lstlast_bonus.c
@@ -25,35 +25,3 @@ t_list	*ft_lstlast(t_list *lst)
 	}
 	return (last);
 }
-/*#include <stdio.h>
-int	main(void)
-{
-	t_list  *head = NULL;
-	t_list  *node1 = ft_lstnew("ABC");
-	t_list  *node2 = ft_lstnew("123");
-	t_list  *node3 = ft_lstnew("XYZ");
-
-
-
-	ft_lstadd_front(&head, node1);
-	ft_lstadd_front(&head, node2);
-	ft_lstadd_front(&head, node3);
-
-
-
-	t_list *temp = head;
-	printf("LISTE KAMLA: ");
-	while (temp != NULL)
-	{
-		printf("%s -> ", (char *)temp->content);
-		temp = temp->next;
-	}
-	printf("NULL\n");
-
-
-
-	t_list *last = ft_lstlast(head);
-	printf("LAST NODE: %s\n", (char *)last->content);
-
-	return (0);
-}*/
