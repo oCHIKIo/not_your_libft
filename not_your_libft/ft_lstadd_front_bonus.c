62f6843454bb15cda9f48ba04bc9f2a54b82bc69
CHIKI <108464117+oCHIKIo@users.noreply.github.com>
2024-12-20 21:59:11 +0100

    Update ft_lstadd_front_bonus.c

--- not_your_libft/ft_lstadd_front_bonus.c
+++ not_your_libft/ft_lstadd_front_bonus.c
@@ -21,20 +21,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = *lst;
 	*lst = new;
 }
-/*#include <stdio.h>
-int	main(void)
-{
-	t_list  *head = NULL;
 
-	t_list  *node1 = ft_lstnew("lwla");
-	t_list  *node2 = ft_lstnew("tanya");
-	t_list  *node3 = ft_lstnew("la5ra");
-
-	ft_lstadd_front(&head, node1);
-	ft_lstadd_front(&head, node2);
-	ft_lstadd_front(&head, node3);
-
-	printf("%s -> %s -> %s\n", (char *)head->content,
 	(char *)head->next->content,
 	(char *)head->next->next->content );
-}*/
+}
